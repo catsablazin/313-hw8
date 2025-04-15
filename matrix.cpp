77a43c1d892cc3de5215ae0788b4e7a32a110b15
Camila <cton2@umbc.edu>
2025-04-15 15:38:55 -0400

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -42,15 +42,48 @@ Matrix scalar(const Matrix& m, int scale) {
   
     Matrix result(rows, vector<int>(cols));
   
-    for (int i = 0; i < rows; ++i)
+    for (int i = 0; i < rows; ++i){
         for (int j = 0; j < cols; ++j)
             result[i][j] = m[i][j] * scale;
+    }
     return result;
 }
 
+Matrix add(const Matrix& a, const Matrix& b) {
+  int rows = a.size();
+  int cols = a[0].size();
 
+  Matrix result(rows, vector<int>(cols));
 
+  for (int i = 0; i < rows; ++i){
+    for (int j = 0; j < cols; ++j)
+      result[i][j] = a[i][j] + b[i][j];
+  }
+  return result;
+}
 
+void print(const Matrix& m) {
+  int rows = m.size();
+  int cols = m[0].size();
+  
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j)
+      cout << m[i][j] << " ";
+    cout << endl;
+  }
+}
 
 int main (){
+  Matrix A = {{6, 4}, {8, 3}};
+  Matrix B = {{1, 2, 3}, {4, 5, 6}};
+  Matrix C = {{2, 4, 6}, {1, 3, 5}};
+
+  Matrix B_scaled = scalarMultiply(B, 3);
+  Matrix C_transposed = transpose(C);
+  Matrix product = multiply(B_scaled, C_transposed);
+  Matrix D = add(A, product);
+
+  cout << "Matrix D:" << endl;
+  printMatrix(D);
+  return 0;
 }
